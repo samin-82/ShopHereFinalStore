13d2a6fa1a577ff0b88bc0495027615f384b4e6b
Samin Asgari <bang1smn1bang@gmail.com>
2025-01-25 19:47:55 +0330

    first commit

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        // protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
